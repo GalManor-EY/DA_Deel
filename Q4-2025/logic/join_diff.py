d00b398666e6210f92a23bb4ece487fd6b423503
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:24:50 +0300

    Add run_join_diff function to join_diff.py

--- Q4-2025/logic/join_diff.py
+++ Q4-2025/logic/join_diff.py
@@ -1,1 +1,12 @@
 
+# logic/join_diff.py
+
+from utils.db_mysql import run_query, load_sql
+import pandas as pd
+
+def run_join_diff():
+    query = load_sql("queries/join_diff.sql")
+    df = run_query(query)
+    df.to_csv("results/join_diff.csv", index=False)
+    print("join_diff completed:", len(df), "rows")
+
