6d0f2c1411ea2a9141fe680e7e4c7c1e46474fab
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:34:06 +0300

    Add MySQL engine creation function

--- Q4-2025/utils/db_mysql.py
+++ Q4-2025/utils/db_mysql.py
@@ -1,1 +1,12 @@
 
+import os
+from sqlalchemy import create_engine
+
+def get_engine():
+    host = os.environ["MYSQL_HOST"]
+    port = os.environ.get("MYSQL_PORT", "3306")
+    user = os.environ["MYSQL_USER"]
+    password = os.environ["MYSQL_PASSWORD"]
+    database = os.environ["MYSQL_DB"]
+
+    return create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
