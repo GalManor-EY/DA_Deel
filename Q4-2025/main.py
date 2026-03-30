a1d1a9427fd359484a554579a98b5614935438e0
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:38:34 +0300

    Refactor imports and update main function calls

--- Q4-2025/main.py
+++ Q4-2025/main.py
@@ -1,10 +1,10 @@
 
-from logic.join_diff_provider import run as run_join_diff_provider
-from logic.payout_not_tms_cutoff import run as run_payout_not_tms_cutoff
-from logic.tms_not_payout_cutoff import run as run_tms_not_payout_cutoff
+from logic.join_diff import run as run_join_diff
+from logic.in_payout_not_tms import run as run_payout_not_tms
+from logic.in_tms_not_payout import run as run_tms_not_payout
 
 def main():
-    run_join_diff_provider()
+    run_join_diff()
     run_payout_not_tms_cutoff()
     run_tms_not_payout_cutoff()
 
