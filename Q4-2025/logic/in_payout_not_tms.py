8de78aff044b41d2775c260f91971b138c5f2a66
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:36:16 +0300

    Add function to run payout not TMS query and save results

--- Q4-2025/logic/in_payout_not_tms.py
+++ Q4-2025/logic/in_payout_not_tms.py
@@ -1,1 +1,10 @@
 
+from utils.db_mysql import get_engine
+from utils.sql_runner import load_sql, run_sql
+
+def run():
+    engine = get_engine()
+    sql = load_sql("queries/payout_not_tms_cutoff.sql")
+    df = run_sql(engine, sql)
+    df.to_csv("results/payout_not_tms_cutoff.csv", index=False)
+    print("payout_not_tms_cutoff rows:", len(df))
