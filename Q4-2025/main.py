cc38a758c2bbdbec2d3037e8be34f3913bee6679
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:37:16 +0300

    Add main function to run multiple logic providers

--- Q4-2025/main.py
+++ Q4-2025/main.py
@@ -1,1 +1,12 @@
 
+from logic.join_diff_provider import run as run_join_diff_provider
+from logic.payout_not_tms_cutoff import run as run_payout_not_tms_cutoff
+from logic.tms_not_payout_cutoff import run as run_tms_not_payout_cutoff
+
+def main():
+    run_join_diff_provider()
+    run_payout_not_tms_cutoff()
+    run_tms_not_payout_cutoff()
+
+if __name__ == "__main__":
+    main()
