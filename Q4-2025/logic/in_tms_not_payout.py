2d637e8c4e61439677f08ca83ff9fea51e24610d
GalManor-EY <Gal.Manor@il.ey.com>
2026-03-30 18:36:48 +0300

    Implement SQL execution and CSV export in in_tms_not_payout
    Add functionality to run SQL query and save results to CSV.

--- Q4-2025/logic/in_tms_not_payout.py
+++ Q4-2025/logic/in_tms_not_payout.py
@@ -1,1 +1,10 @@
+from utils.db_mysql import get_engine
+from utils.sql_runner import load_sql, run_sql
 
+def run():
+    engine = get_engine()
+    sql = load_sql("queries/tms_not_payout_cutoff.sql")
+    df = run_sql(engine, sql)
+    df.to_csv("results/tms_not_payout_cutoff.csv", index=False)
+    print("tms_not_payout_cutoff rows:", len(df))
+``
